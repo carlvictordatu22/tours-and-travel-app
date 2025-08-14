ea34b2f1a924690f528f672eb2d8e050ed93ea02
carlvictordatu22 <carlvictordatu22@gmail.com>
2025-08-15 06:40:32 +0800

    feat: page-specific titles and meta tags

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,24 +2,55 @@ import { Routes } from '@angular/router';
 import { ActivitiesComponent } from './features/activities';
 
 export const routes: Routes = [
-    { path: '', component: ActivitiesComponent, pathMatch: 'full' },
+    {
+        path: '', component: ActivitiesComponent, pathMatch: 'full', data: {
+            title: 'Activities',
+            ogTitle: 'Activities List',
+            ogDescription: 'Check out our amazing list of activities!',
+            ogImage: ''
+        },
+    },
     {
         path: 'hotels',
+        data: {
+            title: 'Hotels',
+            ogTitle: 'Hotel List',
+            ogDescription: 'Check out our amazing list of hotels!',
+            ogImage: ''
+        },
         loadComponent: () => import('./features/hotels/hotels.component')
             .then(mod => mod.HotelsComponent)
     },
     {
         path: 'restaurants',
+        data: {
+            title: 'Restaurants',
+            ogTitle: 'Restaurants List',
+            ogDescription: 'Check out our amazing list of restaurants!',
+            ogImage: ''
+        },
         loadComponent: () => import('./features/restaurants/restaurants.component')
             .then(mod => mod.RestaurantsComponent)
     },
     {
         path: 'ai-search',
+        data: {
+            title: 'AI Search',
+            ogTitle: 'Search Relevant Entries',
+            ogDescription: 'Search for any keywords and we will show relevant entries.',
+            ogImage: ''
+        },
         loadComponent: () => import('./features/ai-search/ai-search.component')
             .then(mod => mod.AISearchComponent)
     },
     {
         path: 'profile',
+        data: {
+            title: 'Profile',
+            ogTitle: 'User Profile',
+            ogDescription: 'Personal space for user to see account related items.',
+            ogImage: ''
+        },
         loadComponent: () => import('./features/profile/profile.component')
             .then(mod => mod.ProfileComponent)
     },
