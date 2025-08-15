838c3cee01888caad4721b69ec36ad022a2893d1
carlvictordatu22 <carlvictordatu22@gmail.com>
2025-08-15 08:25:27 +0800

    feat: initial implementation of material dialog

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { CreateItineraryComponent } from '../create-itinerary';
+import { MODAL_PANEL_CLASS, MODAL_WIDTH } from '../../constants';
 
 @Component({
   selector: 'tnt-navbar',
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, MatDialogModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  readonly #dialog = inject(MatDialog);
+
+  openCreateItineraryDialog(): void {
+    this.#dialog.open(
+      CreateItineraryComponent,
+      {
+        width: MODAL_WIDTH,
+        panelClass: MODAL_PANEL_CLASS,
+        hasBackdrop: true,
+      }
+    );
+  }
+}
